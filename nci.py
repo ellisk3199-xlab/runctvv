2985920da42825953b12ee09da099472aea28573
ellisk3199-xlab <ellisk3199@gmail.com>
2026-05-13 13:00:47 -0700

    Enhance nci.py with data type examples
    Added comments and print statements to demonstrate data types.

--- nci.py
+++ nci.py
@@ -1,2 +1,11 @@
 import wandb
+
 print(f'The version of wandb is:{wandb.__version__}')
+
+
+#comment on data type
+print("hello this is just a script")
+print(type("Spinster unattached not seeking type")) # Type 'string' 
+print(type(3)) # Type 'int'
+print(type(0.33))  # Type 'bfloat'
+
