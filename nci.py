cd274ced1eb20c0bd36640f26280a73a77f7953e
ellisk3199-xlab <ellisk3199@gmail.com>
2026-05-13 14:57:26 -0700

    Update nci.py

--- nci.py
+++ nci.py
@@ -1,11 +1,3 @@
 import wandb
 
 print(f'The version of wandb is:{wandb.__version__}')
-#Comment on data type
-
-def display_it(text):
-  print(text)
-display_it("hello this is just a script")
-display_it(type("seeking friends")) # Type 'string' 
-display_it(type(3)) # Type 'int'
-display_it(type(0.33))  # Type 'bfloat'
